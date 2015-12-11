131df3a733851873de58884f039e42f89bbfea19
likeshumidity <jjoseph03@mica.edu>
2015-12-11 12:33:00 +0000

    updated colors on flow and year to year

--- js/yeartoyear.js
+++ js/yeartoyear.js
@@ -1,3 +1,75 @@
+/* implementation heavily influenced by http://bl.ocks.org/1166403 */
+
+/*
+function updateContent() {
+  // define dimensions of graph
+var m = [80, 80, 80, 80]; // margins
+var w = 1000 - m[1] - m[3]; // width
+var h = 400 - m[0] - m[2]; // height
+
+// create a simple data array that we'll plot with a line (this array
+// represents only the Y values, X will just be the index location)
+var data = [3, 6, 2, 7, 5, 2, 0, 3, 8, 9, 2, 5, 9, 3, 6, 3, 6, 2, 7, 5, 2,
+1, 3, 8, 9, 2, 5, 9, 2, 7];
+//
+// X scale will fit all values from data[] within pixels 0-w
+var x = d3.scale.linear().domain([0, data.length]).range([0, w]);
+// Y scale will fit values from 0-10 within pixels h-0 (Note the inverted
+// domain for the y-scale: bigger is up!)
+var y = d3.scale.linear().domain([0, 10]).range([h, 0]);
+// automatically determining max range can work something like this
+// var y = d3.scale.linear().domain([0, d3.max(data)]).range([h, 0]);
+//
+// create a line function that can convert data[] into x and y points
+var line = d3.svg.line()
+// assign the X function to plot our line as we wish
+.x(function(d,i) { 
+// verbose logging to show what's actually being done
+console.log('Plotting X value for data point: ' + d + ' using index: ' + i +
+' to be at: ' + x(i) + ' using our xScale.');
+// return the X coordinate where we want to plot this datapoint
+return x(i); 
+})
+.y(function(d) { 
+// verbose logging to show what's actually being done
+console.log('Plotting Y value for data point: ' + d + ' to be at: ' + y(d) +
+" using our yScale.");
+// return the Y coordinate where we want to plot this datapoint
+return y(d); 
+})
+//
+// Add an SVG element with the desired dimensions and margin.
+var graph = d3.select(".linechart-yeartoyear").append("svg:svg")
+.attr("width", w + m[1] + m[3])
+.attr("height", h + m[0] + m[2])
+.append("svg:g")
+.attr("transform", "translate(" + m[3] + "," + m[0] + ")");
+//
+// create yAxis
+var xAxis = d3.svg.axis().scale(x).tickSize(-h).tickSubdivide(true);
+// Add the x-axis.
+graph.append("svg:g")
+.attr("class", "x axis")
+.attr("transform", "translate(0," + h + ")")
+.call(xAxis);
+//
+//
+// create left yAxis
+var yAxisLeft = d3.svg.axis().scale(y).ticks(4).orient("left");
+// Add the y-axis to the left
+graph.append("svg:g")
+.attr("class", "y axis")
+.attr("transform", "translate(-25,0)")
+.call(yAxisLeft);
+//
+// Add the line by appending an svg:path element with the data line we
+//created above
+// do this AFTER the axes above so that the line is above the tick-lines
+graph.append("svg:path").attr("d", line(data));
+//
+}
+*/
+
 $(document).ready(function () {
   updateContent();
 });
@@ -17,7 +89,7 @@ function updateContent() {
   var y = d3.scale.linear()
     .range([height, 0]);
 
-  var color = d3.scale.category20b();
+  var color = d3.scale.category20();
 
   var xAxis = d3.svg.axis()
     .scale(x)
@@ -55,19 +127,18 @@ function updateContent() {
 
   svg.append("g")
     .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
+    .attr("transform", "translate(-" + margin.left +"," + height + ")")
     .call(xAxis);
 
   svg.append("g")
     .attr("class", "y axis")
     .call(yAxis)
-/*
-    .append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 6)
-    .attr("dy", ".71em")
-    .style("text-anchor", "end");
-*/
+    .append('text')
+    .attr('transform', 'rotate(-90)')
+    .attr('y', 6)
+    .attr('dy', '.71em')
+    .style('text-anchor', 'end')
+    .text('$ in thousands');
 
   var series = svg.selectAll(".branch")
     .data(data)
@@ -83,6 +154,13 @@ function updateContent() {
       return color(d.series);
     })
     .style("fill", "none");
+
+  series.append('text')
+    .datum(function(d) { return { name: d.year, value: d.values[d.values.length - 1]}; })
+    .attr('transform', function(d) { return 'translate(' + x(d.value.period) + "," + y(d.value.value) + ")"; })
+    .attr('x', 3)
+    .attr('dy', '.35em')
+    .text(function(d) { return d.name; });
 }
 
 function convertForLine(inData) {
@@ -125,4 +203,6 @@ function convertForLine(inData) {
 
   return outData;
 }
+/*
+*/
 
